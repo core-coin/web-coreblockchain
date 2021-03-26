4cbf6ecdf0a16903f4a467cf364cc390eb8f9f2e
NataKrass <nat14@i.ua>
2021-03-26 15:45:19 +0100

    changed mark tag

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,8 +1,8 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import docs from '../../markdown-pages/docs.json'
 import lunr from 'lunr'
 import { keyBy } from 'lodash'
-import { Marker } from 'react-mark.js'
+import Mark from 'mark.js'
 import './Search.scss'
 
 import SearchIcon from '../SvgIcon/icons/Search'
@@ -28,46 +28,53 @@ var idx = lunr(function () {
 
 const SearchBar = () => {
   const [results, setResults] = useState([])
-  let [isOpen, setIsOpen] = useState(false)
   let [value, setValue] = useState([])
+  var markInstance = new Mark(document.querySelector('.search'))
 
-  function handleStatusChange() {
-    setIsOpen(!isOpen)
-  }
+  useEffect(() => {
+    setTimeout(() => {
+      markInstance.unmark({
+        done: function() {
+          markInstance.mark(value, {
+            element: 'em'
+          })
+        }
+      })
+    }, 0)
+  }, [value])
 
   return (
-    <div className="search">
-      <label className="search_box search_box__open">
+    <div className='search'>
+      <label className='search_box search_box__open'>
         <input
-          type="search"
-          placeholder="Search for a term or phrase..."
-          name="search"
+          type='search'
+          placeholder='Search for a term or phrase...'
+          name='search'
           autoFocus={true}
-          className="search_box__input"
+          className='search_box__input'
           onChange={useCallback((e) => {
             const res = idx.search(`${e.target.value}`)
             const searchRes = res.map((i) => index[i.ref])
             value = e.target.value
+
             setValue(value)
             setResults(searchRes)
+         
           }, [])}
         />
-        <span className="search_box__icon" onClick={handleStatusChange}>
+        <span className='search_box__icon'>
           <SearchIcon />
         </span>
       </label>
-      <div className="results" id="results">
+      <div className='results' id='results'>
         {results.map((doc) => (
-           <Marker mark={value}>
           <a href={doc.slug} key={doc.basename}>
             <h3>{doc.title}</h3>
             <p>
               {doc.description.substr(0, 280)}
               ...
             </p>
-          
           </a>
-          </Marker>
         ))}
       </div>
     </div>
