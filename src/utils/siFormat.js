36f307fd2b1872f231a768717d8cb4a6496c4eb9
Alejandro Barthelemy Ortega <aabo970627@gmail.com>
2022-05-08 10:54:35 -0400

    some fixes

--- src/utils/siFormat.js
+++ src/utils/siFormat.js
@@ -8,11 +8,12 @@ export default function siFormat(num, digits) {
     { value: 1E15, symbol: "P" },
     { value: 1E18, symbol: "E" }
   ]
-  const rx = /\.0+$|(\.[0-9]*[1-9])0+$/
+  // const rx = /\.0+$|(\.[0-9]*[1-9])0+$/
   for (var i = si.length - 1; i > 0; i--) {
     if (num >= si[i].value) {
       break
     }
   }
-  return (num / si[i].value).toFixed(digits).replace(rx, "$1") + ' ' + si[i].symbol
+  // return (num / si[i].value).toFixed(digits).replace(rx, "$1") + ' ' + si[i].symbol
+  return (num / si[i].value).toFixed(digits)
 }
